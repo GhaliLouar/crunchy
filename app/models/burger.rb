d976821a6305868a4d4e1515531a185bc867b33a
Ghali Louar <ghalilouarc@gmail.com>
2023-03-02 20:38:15 +0100

    crunchy

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -2,7 +2,4 @@ class Burger < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :image
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
